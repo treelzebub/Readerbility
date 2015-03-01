3c9ee99609a3aa29c4fdefe8ba7a381561f8c41f
Tre Murillo <tre.murillo@gmail.com>
2015-02-28 18:19:18 -0600

    Initial commit

--- app/src/main/java/com/treelzebub/readerbility/ui/fragtivity/Login.java
+++ app/src/main/java/com/treelzebub/readerbility/ui/fragtivity/Login.java
@@ -1,5 +1,6 @@
 package com.treelzebub.readerbility.ui.fragtivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -19,12 +20,11 @@ public class Login {
         public static final String TAG = "login_fragment";
 
         private static LoginFragment loginFragment;
-        private static LayoutInflater mInflater;
 
         public LoginFragment() {
         }
 
-        public static Fragment getInstance() {
+        public static Fragment getInstance(Context c) {
             if (loginFragment == null) {
                 loginFragment = new LoginFragment();
             }
@@ -38,11 +38,10 @@ public class Login {
             super.onCreateView(inflater, container, savedInstanceState);
 
             View v;
-            v = mInflater.inflate(R.layout.fragment_login, container, false);
+            v = inflater.inflate(R.layout.fragment_login, container, false);
 
             EditText usernameEdit = (EditText) v.findViewById(R.id.username_edit);
             EditText pwdEdit = (EditText) v.findViewById(R.id.pwd_edit);
-
 
             return v;
         }
